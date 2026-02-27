eb03cd80b7c92bee9e892eed8bd0c4b57de0a7b7
Ryan keogh <145026596+Tr0klol@users.noreply.github.com>
2026-02-27 11:50:25 +0000

    Add files via upload

--- compsciinvest/artefact/main.py
+++ compsciinvest/artefact/main.py
@@ -1,12 +1,12 @@
-serial.write_line("time_ms,temp,gas,soil,light,uv,risk")
+serial.write_line("time_ms,temp,gas,soil,light,uv,risk") 
 while True:
     time_ms = control.millis() / 1000
     temp = pins.analog_read_pin(AnalogPin.P0) / 10
     soil = pins.analog_read_pin(AnalogPin.P1)
     gas = pins.digital_read_pin(DigitalPin.P2)
     light_val = pins.analog_read_pin(AnalogPin.P3)
-    uv = pins.analog_read_pin(AnalogPin.P4)
-    risk = 0
+    uv = pins.analog_read_pin(AnalogPin.P4) # Basic req 2: 
+    risk = 0 #Basic req 3: #Adv req 1
     if temp < 400:
         risk += 0
     elif temp < 600:
@@ -32,7 +32,7 @@ while True:
     if uv > 900:
         risk += 10
     elif uv > 700:
-        risk += 5
+        risk += 5 #Adv req 2
     line = "" + str(time_ms) + "," + ("" + str(temp)) + "," + ("" + str(soil)) + "," + ("" + str(gas)) + "," + ("" + str(light_val)) + "," + ("" + str(uv)) + "," + ("" + str(risk))
     serial.write_line(line)
     if risk >= 60:
@@ -41,7 +41,7 @@ while True:
         basic.show_icon(IconNames.CONFUSED)
     else:
         basic.show_icon(IconNames.HEART)
-    if risk >= 80:
+    if risk >= 80: #adv req 3
         music.play(music.tone_playable(440, music.beat(BeatFraction.WHOLE)),
             music.PlaybackMode.UNTIL_DONE)
     elif risk >= 60:
